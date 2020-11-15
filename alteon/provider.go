3d1312e372cdafc420e87ca08b072ad1ad55d6a0
iromaniuk@advisor360.com <docker@PF1L54YP.advisor360corp.com>
2020-11-15 17:55:35 -0500

    Updated

--- alteon/provider.go
+++ alteon/provider.go
@@ -42,9 +42,14 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
   
 	// Warning or errors can be collected in a slice type
 	var diags diag.Diagnostics
+	diags = append(diags, diag.Diagnostic{
+		Severity: diag.Warning,
+		Summary:  "Warning Message Summary",
+		Detail:   "This is the detailed warning message from providerConfigure",
+	})
   
 	if (username != "") && (password != "") {
-	  c, err := alteon.NewClient(uri, &username, &password)
+	  c, err := alteon.NewClient(&uri, &username, &password)
 	  if err != nil {
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/irekromaniuk/terraform-provider-alteon
 
 go 1.15
+
+require (
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.2.0
+	github.com/irekromaniuk/alteon-client-go v0.0.0-20201115222328-956d6a8a9123
+)
